6b5c8db6ff0b447af08f42c62da6c720a6682d70
Kiminaze <kiminaze@yahoo.de>
2020-08-02 06:23:59 +0200

    Car is gud now

--- Assets/Scripts/SimpleCarController.cs
+++ Assets/Scripts/SimpleCarController.cs
@@ -13,33 +13,96 @@ public class AxleInfo
 
 public class SimpleCarController : MonoBehaviour
 {
+    [SerializeField]
+    private Rigidbody rb;
+
     public List<AxleInfo> axleInfos;
-    public float maxMotorTorque;
-    public float maxSteeringAngle;
-    public float currentMotorTorque;
-    public float currentSteeringAngle;
+    public float maxMotorTorque = 1000f;
+    public float maxSteeringAngle = 35f;
 
-    // finds the corresponding visual wheel
+    public bool isPulling = false;
+    public float pullStrength = 0f;
+    public float pullStrengthMultiplier = 50f;
+    public float pullWeakenMultiplier = 10f;
+    public float slowDownMultiplier = 10f;
+    public float pullToTorqueMultiplier = 10f;
 
-    public void FixedUpdate()
-    {
-        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+    public float currentTorque = 0f;
+
+    /// <summary>
+    /// Just for displaying velocity in inspector
+    /// </summary>
+    public float currentVelocity;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            isPulling = true;
+        } else if (Input.GetKeyUp(KeyCode.S)) {
+            isPulling = false;
+
+            // set velocity to forward at current pullStrength
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, pullStrength / 10f);
+        }
+
+        if (isPulling)
+            // Motor "aufziehen" (Feder wird stärker)
+            pullStrength += Time.deltaTime * pullStrengthMultiplier;
+        else
+            // Motor "auslaufen lassen" (Feder wird schwächer)
+            pullStrength -= Time.deltaTime * pullWeakenMultiplier;
+
+        // clamp from 0-100%
+        pullStrength = Mathf.Clamp(pullStrength, 0f, 100f);
+    }
+
+    public void FixedUpdate() {
+        if (!isPulling) {
+            // set motor torque to 
+            currentTorque = pullStrength * pullToTorqueMultiplier;
+        } else {
+            currentTorque -= Time.deltaTime * slowDownMultiplier;
+
+            // this is no good yet
+            rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, -10f);
+        }
+        // clamp torque
+        currentTorque = Mathf.Clamp(currentTorque, 0f, maxMotorTorque);
+
         float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
 
-        foreach (AxleInfo axleInfo in axleInfos)
-        {
-            if (axleInfo.steering)
-            {
-                currentSteeringAngle = steering;
+        foreach (AxleInfo axleInfo in axleInfos) {
+            if (axleInfo.steering) {
                 axleInfo.leftWheel.steerAngle = steering;
                 axleInfo.rightWheel.steerAngle = steering;
             }
-            if (axleInfo.motor)
-            {
-                currentMotorTorque = motor;
-                axleInfo.leftWheel.motorTorque = motor;
-                axleInfo.rightWheel.motorTorque = motor;
+            
+            if (axleInfo.motor) {
+                axleInfo.leftWheel.motorTorque = currentTorque;
+                axleInfo.rightWheel.motorTorque = currentTorque;
             }
+
+            ApplyLocalPositionToVisuals(axleInfo.leftWheel);
+            ApplyLocalPositionToVisuals(axleInfo.rightWheel);
         }
+
+        // display velocity in inspector
+        currentVelocity = rb.velocity.z;
+    }
+
+    // finds the corresponding visual wheel
+    // correctly applies the transform
+    public void ApplyLocalPositionToVisuals(WheelCollider collider) {
+        if (collider.transform.childCount == 0) {
+            return;
+        }
+
+        Transform visualWheel = collider.transform.GetChild(0);
+
+        Vector3 position;
+        Quaternion rotation;
+        collider.GetWorldPose(out position, out rotation);
+
+        visualWheel.transform.position = position;
+        visualWheel.transform.rotation = rotation;
     }
 }

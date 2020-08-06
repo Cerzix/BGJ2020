e964535944df974d27b5ee81ce008ebf1b71eb0f
Cerzix <erik-zellingen@web.de>
2020-08-06 17:49:25 +0200

    Added Tweening Framework, Added Title Polish

---
+++ Assets/Scripts/TitleAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TitleAnimation : MonoBehaviour
+{
+    [SerializeField]
+    private Image gameTitle;
+
+    [SerializeField]
+    private Vector3 punchScale;
+
+    [SerializeField]
+    private float duration;
+
+    [SerializeField]
+    private int vibrato;
+
+    [SerializeField]
+    private float randomness;
+
+    private void Start()
+    {
+        InvokeRepeating("MenuBounce", 0, 1);        
+    }
+
+    private void MenuBounce()
+    {
+        gameTitle.transform.DOPunchScale(punchScale, duration, vibrato, randomness);
+    }
+}

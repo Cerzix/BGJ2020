81bd3156f3a22784484df57c2f12a71c2801decd
Cerzix <erik-zellingen@web.de>
2020-08-06 22:28:18 +0200

    Added Game BGM

---
+++ Assets/Scripts/BGScroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    [SerializeField]
+    private float scrollSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        float yVal = this.transform.position.y;
+
+        this.transform.position = new Vector3(this.transform.position.x, yVal -= scrollSpeed * Time.deltaTime, this.transform.position.z);
+
+        if(this.transform.position.y <= -1000)
+        {
+            this.transform.position = new Vector3(this.transform.position.x, 0, this.transform.position.z);
+        }
+    }
+}

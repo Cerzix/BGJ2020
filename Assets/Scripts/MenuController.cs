a76c1f4fc63ef64dd21387c7c6f6d15f1373fd97
Cerzix <erik-zellingen@web.de>
2020-08-06 15:31:56 +0200

    Added Map

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    [SerializeField]
+    private Button startButton;
+
+    private void Start()
+    {
+        startButton.onClick.AddListener(OnClick);
+    }
+
+    private void OnClick()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using Cinemachine;
 
 public class UIManager : MonoBehaviour
 {
@@ -21,9 +22,28 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     private TrailRenderer trail2;
 
+    [SerializeField]
+    private CinemachineVirtualCamera vCam;
+
+    private CinemachineBasicMultiChannelPerlin cameraNoise;
+
+    private void Start()
+    {
+        cameraNoise = vCam.GetCinemachineComponent<Cinemachine.CinemachineBasicMultiChannelPerlin>();
+    }
+
     private void Update() {
         pullStrengthImg.fillAmount = car.PullStrength / 100;
         pullStrengthText.text = Mathf.RoundToInt(car.PullStrength).ToString();
+
+        if(car.PullStrength > 95 && cameraNoise.m_AmplitudeGain == 0 && cameraNoise.m_AmplitudeGain < 1.5)
+        {
+            cameraNoise.m_AmplitudeGain += 0.5f;
+        }
+        else if(car.PullStrength < 95 && cameraNoise.m_AmplitudeGain > 0)
+        {
+            cameraNoise.m_AmplitudeGain -= 0.1f;
+        }
 
         if(car.PullStrength > 5 && trail1.time <= .3f && trail2.time <= .3f && car.currentVelocity > 0)
         {

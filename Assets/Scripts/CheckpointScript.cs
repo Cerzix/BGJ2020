ec884fe72d78c783ff4afdb8eae91f8f6ddcde47
Cerzix <erik-zellingen@web.de>
2020-08-13 19:27:46 +0200

    Built Final Build

---
+++ Assets/Scripts/CheckpointScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointScript : MonoBehaviour
+{
+    [SerializeField]
+    private float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.transform.rotation;
+    }
+}
